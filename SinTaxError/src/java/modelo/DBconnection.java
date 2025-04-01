b4f795bdecb88b12d99305236a99989dbbed9e2d
CalebAltuna <web24cvalls@imaltuna.com>
2025-04-01 12:23:49 +0200

    new db connect

--- SinTaxError/src/java/modelo/DBconnection.java
+++ SinTaxError/src/java/modelo/DBconnection.java
@@ -1,9 +1,9 @@
 package modelo;
 
 public class DBconnection {
-private static final String URL = "jdbc:mysql://localhost:3306/miBaseDeDatos";
+private static final String URL = "jdbc:mysql://localhost:3306/securedb";
 private static final String USER = "root";
-private static final String PASSWORD ="";
+private static final String PASSWORD ="12345678";
 
 public static Connection getConnection() throws SQLException {
     try {
