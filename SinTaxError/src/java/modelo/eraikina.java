3a63017c5e7a70327d65f19d963170dbee6ccbc8
CalebAltuna <web24cvalls@imaltuna.com>
2025-04-01 08:40:22 +0200

    new java files

---
+++ SinTaxError/src/java/modelo/eraikina.java
@@ -0,0 +1,48 @@
+
+public class Eraikina {
+    int idEraikina;
+    String izenLuzea;
+    String izenLaburra;
+    String helbidea;
+    String postaKodea;
+
+    public Eraikina(int idEraikina, String izenLuzea, String izenLaburra, String helbidea, String postaKodea) {
+        this.idEraikina = idEraikina;
+        this.izenLuzea = izenLuzea;
+        this.izenLaburra = izenLaburra;
+        this.helbidea = helbidea;
+        this.postaKodea = postaKodea;
+    }
+
+    
+    public int getIdEraikina() {
+        return idEraikina;
+    }
+    public void setIdEraikina(int idEraikina) {
+        this.idEraikina = idEraikina;
+    }
+    public String getIzenLuzea() {
+        return izenLuzea;
+    }
+    public void setIzenLuzea(String izenLuzea) {
+        this.izenLuzea = izenLuzea;
+    }
+    public String getIzenLaburra() {
+        return izenLaburra;
+    }
+    public void setIzenLaburra(String izenLaburra) {
+        this.izenLaburra = izenLaburra;
+    }
+    public String getHelbidea() {
+        return helbidea;
+    }
+    public void setHelbidea(String helbidea) {
+        this.helbidea = helbidea;
+    }
+    public String getPostaKodea() {
+        return postaKodea;
+    }
+    public void setPostaKodea(String postaKodea) {
+        this.postaKodea = postaKodea;
+    }
+}
