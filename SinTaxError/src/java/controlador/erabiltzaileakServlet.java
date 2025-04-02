c3de0a82dac814b07f7079fb77e7d53e9822d7b3
CalebAltuna <web24cvalls@imaltuna.com>
2025-04-02 09:44:08 +0200

    function name changed

--- SinTaxError/src/java/controlador/erabiltzaileakServlet.java
+++ SinTaxError/src/java/controlador/erabiltzaileakServlet.java
@@ -8,18 +8,25 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 public class erabiltzaileakServlet {
-	public boolean loginTry(Erabiltzaileak erabiltzailea) {
-		boolean vuelta = false;
-		try {
-			if(erabiltzailea.getIzena().equals(erabiltzaileakDAO.devolverNombreBaseDeDatos(erabiltzailea.getIzena())) && erabiltzailea.getPassword().equals(erabiltzaileakDAO.devolverPasswordBaseDeDatos(erabiltzailea.getIzena()))) {
-				vuelta = true;
-			}
-			
-		}catch(NullPointerException e) {
-			vuelta = false;
-		}
-		return vuelta;
+	public boolean recogerDatos(Erabiltzaileak erabiltzailea) {
+	    boolean vuelta = false;
+	    try {
+	        String nombreBD = erabiltzaileakDAO.devolverNombreBaseDeDatos(erabiltzailea.getIzena());
+	        String passwordBD = erabiltzaileakDAO.devolverPasswordBaseDeDatos(erabiltzailea.getIzena());
+	
+	        if (erabiltzailea.getIzena().equals(nombreBD) && erabiltzailea.getPassword().equals(passwordBD)) {
+	            vuelta = true;
+	        }
+	    } catch (NullPointerException e) {
+	        System.err.println("NullPointerException: " + e.getMessage());
+	        vuelta = false;
+	    } catch (Exception e) {
+	        System.err.println("Exception: " + e.getMessage());
+	        vuelta = false;
+	    }
+	    return vuelta;
 	}
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		String nombreUsuario = request.getParameter("name");
