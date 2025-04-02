25be98be737b3f8317d949b5b5184b2fe47003a0
CalebAltuna <web24cvalls@imaltuna.com>
2025-04-02 09:34:58 +0200

    login?

--- SinTaxError/src/java/controlador/erabiltzaileakServlet.java
+++ SinTaxError/src/java/controlador/erabiltzaileakServlet.java
@@ -1,4 +1,18 @@
+package controlador;
+
+import Modeloa.Erabiltzaileak;
 
 public class erabiltzaileakServlet {
-
+	public boolean loginTry(Erabiltzaileak erabiltzailea) {
+		boolean vuelta = false;
+		try {
+			if(erabiltzailea.getIzena().equals(erabiltzaileakDAO.devolverNombreBaseDeDatos(erabiltzailea.getIzena())) && erabiltzailea.getPassword().equals(erabiltzaileakDAO.devolverPasswordBaseDeDatos(erabiltzailea.getIzena()))) {
+				vuelta = true;
+			}
+			
+		}catch(NullPointerException e) {
+			vuelta = false;
+		}
+		return vuelta;
+	}
 }
