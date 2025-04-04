68a51ad04cc6f927364fb0f655ddf0f934d69dc9
CalebAltuna <web24cvalls@imaltuna.com>
2025-04-04 08:36:10 +0200

    login okey

--- SinTaxError/src/main/java/controlador/erabiltzaileakServlet.java
+++ SinTaxError/src/main/java/controlador/erabiltzaileakServlet.java
@@ -14,40 +14,39 @@ import java.util.logging.Logger;
 
 @WebServlet("/erabiltzaileakServlet")
 public class erabiltzaileakServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-	private static final Logger logger = Logger.getLogger(erabiltzaileakServlet.class.getName());
+    private static final long serialVersionUID = 1L;
+    private static final Logger logger = Logger.getLogger(erabiltzaileakServlet.class.getName());
 
+    public boolean recogerDatos(erabiltzaileak erabiltzailea) {
+        boolean vuelta = false;
+        try {
+            String nombreBD = erabiltzaileakDAO.devolverNombreBaseDeDatos(erabiltzailea.getIzena());
+            String passwordBD = erabiltzaileakDAO.devolverPasswordBaseDeDatos(erabiltzailea.getIzena());
 
-public boolean recogerDatos(erabiltzaileak erabiltzailea) {
-    boolean vuelta = false;
-    try {
-        String nombreBD = erabiltzaileakDAO.devolverNombreBaseDeDatos(erabiltzailea.getIzena());
-        String passwordBD = erabiltzaileakDAO.devolverPasswordBaseDeDatos(erabiltzailea.getPasahitza());
+            logger.log(Level.INFO, "NombreBD: {0}, PasswordBD: {1}", new Object[]{nombreBD, passwordBD});
+            logger.log(Level.INFO, "NombreUsuario: {0}, PasswordUsuario: {1}", new Object[]{erabiltzailea.getIzena(), erabiltzailea.getPasahitza()});
 
-        logger.log(Level.INFO, "NombreBD: {0}, PasswordBD: {1}", new Object[]{nombreBD, passwordBD});
-        logger.log(Level.INFO, "NombreUsuario: {0}, PasswordUsuario: {1}", new Object[]{erabiltzailea.getIzena(), erabiltzailea.getPasahitza()});
-
-        if (erabiltzailea.getIzena().equals(nombreBD) && erabiltzailea.getPasahitza().equals(passwordBD)) {
-            vuelta = true;
+            if (nombreBD != null && passwordBD != null && erabiltzailea.getIzena().equals(nombreBD) && erabiltzailea.getPasahitza().equals(passwordBD)) {
+                vuelta = true;
+            }
+        } catch (NullPointerException e) {
+            logger.log(Level.SEVERE, "NullPointerException: {0}", e.getMessage());
+            vuelta = false;
+        } catch (Exception e) {
+            logger.log(Level.SEVERE, "Exception: {0}", e.getMessage());
+            vuelta = false;
         }
-    } catch (NullPointerException e) {
-        logger.log(Level.SEVERE, "NullPointerException: {0}", e.getMessage());
-        vuelta = false;
-    } catch (Exception e) {
-        logger.log(Level.SEVERE, "Exception: {0}", e.getMessage());
-        vuelta = false;
+        return vuelta;
     }
-    return vuelta;
-}
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
 
-        erabiltzaileak erabiltzailea = new erabiltzaileak(username, password);
+        erabiltzaileak erabiltzaile = new erabiltzaileak(username, password);
 
-        if (recogerDatos(erabiltzailea)) {
-            response.sendRedirect("AdminDB.jsp");
+        if (recogerDatos(erabiltzaile)) {
+            response.sendRedirect("AdminDB1.jsp");
         } else {
             response.sendRedirect("try.jsp");
         }
