2045de92bf4e81e208a3b7d1c41f7b00ecea0f2e
CalebAltuna <web24cvalls@imaltuna.com>
2025-04-08 09:34:29 +0200

    delete erab

--- SinTaxError/src/main/java/modelo/erabiltzaileaDAO.java
+++ SinTaxError/src/main/java/modelo/erabiltzaileaDAO.java
@@ -4,19 +4,32 @@ import java.sql.ResultSet;
 import java.sql.Connection;
 import java.util.ArrayList;
 public class erabiltzaileaDAO {
-    public void insertErabiltzailea(erabiltzailea e) {
+    public void insertErabiltzailea(erabiltzailea erab) {
         try {
             Connection conn = DBconnection.getConnection();
             String sql = "INSERT INTO usuarios (ID_Erabiltzailea,Pasahitza,Rola,Izena) VALUES (?,?,?,?)";
             PreparedStatement pst = conn.prepareStatement(sql);
-            pst.setInt(1, e.getID_Erabiltzailea());
-            pst.setString(2, e.getIzena());
-            pst.setString(3, e.getPasahitza());
-            pst.setString(4, e.getRola());
+            pst.setInt(1, erab.getID_Erabiltzailea());
+            pst.setString(2, erab.getIzena());
+            pst.setString(3, erab.getPasahitza());
+            pst.setString(4, erab.getRola());
             pst.execute();
             DBconnection.desConnection();
-        } catch (Exception ex) {
-            System.out.println("Error: " + ex.getMessage());
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
         }
     }
+
+	public void deleteErabiltzailea(int id) {
+		try {
+			Connection conn = DBconnection.getConnection();
+			String sql = "DELETE FROM usuarios WHERE ID_Erabiltzailea=?";
+			PreparedStatement pst = conn.prepareStatement(sql);
+			pst.setInt(1, id);
+			pst.execute();
+			DBconnection.desConnection();
+		} catch (Exception e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+	}
 }
